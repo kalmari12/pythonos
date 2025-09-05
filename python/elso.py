e78db68bdf5fbbf07fc2564369ef59db0f02e8b3
Székely András <andrisszb23@gmail.com>
2025-09-05 10:46:51 +0200

    Add files via upload

---
+++ python/elso.py
@@ -0,0 +1,28 @@
+szam1 = int(input("Kérem az első számot: "))
+szam2 = int(input("Kérem a második számot: "))
+
+ossz = szam1 + szam2
+kiv = szam1 - szam2
+szorz = szam1 * szam2
+hanyad = szam1 / szam2
+telje = ossz + kiv + szorz + hanyad
+
+print(f"\t<< Eredmények >>\n\n"
+      f"\tA két szám összege: {ossz}\n"
+      f"\tA két szám különbsége: {kiv}\n"
+      f"\tA két szám szorzata: {szorz}\n"
+      f"\tA két szám hányadosa: {hanyad}\n"
+      f"\tAz összes műveletnek az egybevett összege: {telje}\n\n"
+      f"\t<< Vége >>"  )
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python/harmadik.py
@@ -0,0 +1,11 @@
+import math
+
+szam1 = int(input("Kérek egy számot: "))
+
+hatvány = szam1 ** 2
+
+gyök = math.sqrt 
+
+print(f"A szám hatványa: {hatvány}"
+      f"A szám gyöke: {gyök}")
+
