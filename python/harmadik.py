dbe5a6a9735721bb26c078182a72c829054b0bcc
kalmari12 <andrisszb23@gmail.com>
2025-09-05 10:48:34 +0200

    kis valtoztatasok

--- python/harmadik.py
+++ python/harmadik.py
@@ -4,8 +4,8 @@ szam1 = int(input("Kérek egy számot: "))
 
 hatvány = szam1 ** 2
 
-gyök = math.sqrt 
+gyök = int(math.sqrt(szam1)) 
 
-print(f"A szám hatványa: {hatvány}"
+print(f"A szám hatványa: {hatvány}\n"
       f"A szám gyöke: {gyök}")
 
