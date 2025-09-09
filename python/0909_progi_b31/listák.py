d7f46f978781e7705c38d34331e357a9870a0079
kalmari12 <andrisszb23@gmai.com>
2025-09-09 13:17:56 +0200

    uccso

---
+++ python/0909_progi_b31/listák.py
@@ -0,0 +1,8 @@
+szöveg = str("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lobortis, dolor sed volutpat dictum, odio nisl iaculis nulla, eget aliquet tortor ligula ac enim. Nulla vel dictum erat, sit amet convallis nunc. Nunc fringilla justo nec purus volutpat, eget gravida tellus mollis. Suspendisse tincidunt lacinia tempor. Curabitur id elit fringilla, elementum leo et, vestibulum lacus. Maecenas vitae blandit libero. Nullam sit amet sapien vel neque aliquet pellentesque nec eu neque. Quisque nec ipsum lacus. Cras eleifend nibh nibh, vel consequat nulla iaculis a. Cras vitae ultricies arcu. Duis urna dolor, imperdiet vitae commodo ac, iaculis at sapien. Cras sed commodo justo, sed fringilla est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu eros, tincidunt non enim eu, bibendum ullamcorper metus.")
+
+print(f"\t\n<< Eredmények >>\n"
+      f"A szöveg hossza: {len(szöveg)}\n\n"
+      f"Az elsö betű: {szöveg[0]}\n\n"
+      f" Az utolsó karakter: {szöveg[-1]}")
+
+
