c69efe586077cadf8604eb3a6750a4bd96201185
kalmari12 <andrisszb23@gmai.com>
2025-09-23 12:33:45 +0200

    alapok fent

---
+++ python/0923_progi_b31/bool.py/bool.py
@@ -0,0 +1,21 @@
+a = bool(True)
+
+b = int(5)
+
+c = int(12)
+
+e1 = b >= 12
+
+e2 = b <= c
+
+e3 = b == c
+
+e4 = b != c
+
+print(f"az e1: {e1}")
+print(f"Az e2: {e2}")
+
+if e2 == True:
+    print(f" Az állítás igaz!")
+
+
