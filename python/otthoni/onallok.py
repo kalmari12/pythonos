4c96da9891952ac98ecdbe18b140eb1cc5b897fb
kalmari12 <andrisszb23@gmail.com>
2025-10-15 21:28:55 +0200

    otthoniak fent

---
+++ python/otthoni/onallok.py
@@ -0,0 +1,6 @@
+#dobalj egy hat oldalu dobokockaval amig nem dobsz hatot, amig a dobasok osszege el nem eri a szazat, amig a dobott szam nem lesz paratlan, amig nem dobsz ketszer egymas utan ugyanakkorat, ezeket kulon kulon
+
+
+
+
+
