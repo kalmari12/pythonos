8fe187447a2cc5fbae2dd800d32d81c5615bf524
kalmari12 <andrisszb23@gmai.com>
2025-10-21 13:11:49 +0200

    asdasda

---
+++ python/10.21_B31_progi/sulipy356.py
@@ -0,0 +1,18 @@
+'''lista = []
+nev = None
+
+while nev != '' and len(lista) != 3: 
+    nev = str(input("kerek egy kersztnevet"))
+    if nev != '' and len(lista) != 3:
+        lista.append(nev)
+
+print(lista)
+
+'''
+
+kislista = []
+
+
+
+
+
