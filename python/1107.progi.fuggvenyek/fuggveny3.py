72ba37a1333a99e9cc2619e134fa6ee558f8aec9
Székely András <andrisszb23@gmail.com>
2025-11-07 11:26:06 +0100

    rakene nezni

--- python/1107.progi.fuggvenyek/fuggveny3.py
+++ python/1107.progi.fuggvenyek/fuggveny3.py
@@ -14,7 +14,7 @@ for i in range(len(szamlista) - 2):
     if szamlista[i] == 1 and szamlista[i+1] == 1 and szamlista[i+2] == 1:
         index+= 1
 
-print(index)
+print(f"A 111 minta ennyiszer fordul elo a listaban: {index}")
 
 
 
